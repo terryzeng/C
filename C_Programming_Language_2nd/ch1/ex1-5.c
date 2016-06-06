b930652e434b0696a527e82a89836017e34d86be
terryzeng <terryzeng@Note3403>
2016-06-06 14:04:15 -0400

    Add new file

--- C_Programming_Language_2nd/ch1/ex1-5.c
+++ C_Programming_Language_2nd/ch1/ex1-5.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
-#define LOWER 0
-#define UPPER 300
-#define STEP  20
-
-main()
+int main()
 {
     int fahr;
     
     printf("  F         C\n");
-    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP )
+    for (fahr = 300; fahr >= 0; fahr = fahr - 20)
     {
         printf("%3d \t %6.1f \n", fahr, (5.0/9.0) * (fahr - 32));
     }
+    return 0;
+    
 }
